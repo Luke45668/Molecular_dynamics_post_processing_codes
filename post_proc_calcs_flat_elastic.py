88bf5b165bbc62fe831a79cea075b14b10403363
Luke45668 <99890053+Luke45668@users.noreply.github.com>
2023-12-06 16:45:42 +0000

    got error matrix to work

--- post_proc_calcs_flat_elastic.py
+++ post_proc_calcs_flat_elastic.py
@@ -51,7 +51,7 @@ TP_general_name_string='temp.'+fluid_name+'**'
 dump_general_name_string='fluid_name'+'**.dump'
 
 
-filepath='KATHLEEN_LAMMPS_RUNS/flat_elastic_tests'
+filepath='KATHLEEN_LAMMPS_RUNS/flat_elastic_tests/test_1'
 
 realisation_name_info= VP_and_momentum_data_realisation_name_grabber(TP_general_name_string,log_general_name_string,VP_general_name_string,Mom_general_name_string,filepath,dump_general_name_string)
 realisation_name_Mom=realisation_name_info[0]
@@ -71,34 +71,41 @@ count_TP=realisation_name_info[9]
 def log_error_detector(filepath,var_choice_1,var_choice_2,realisation_name_log, count_log):
     os.chdir('/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/'+filepath)
     error_matrix=np.zeros((1,var_choice_1.size,var_choice_2.size))
-    general_error_message=bytes("ERROR",'utf-8')
+    general_error_message=bytes("Loop time",'utf-8') # to check if the simulation finished 
 
 
     for i in range(0,count_log):
         split_name_for_org =  realisation_name_log[i].split('_')
-        var_choice_1_posn= split_name_for_org[17] # need 
-        var_choice_2_posn= split_name_for_org[10]
-        realisation_posn= split_name_for_org[6]
-        var_choice_1_index= np.where(var_choice_1_posn==var_choice_1)
-        var_choice_2_index= np.where(var_choice_2_posn==var_choice_2)
-        realisation_index_= np.where(realisation_posn==realisation_index)
+        var_choice_1_posn= float(split_name_for_org[17]) # need 
+        #print(var_choice_1_posn)
+        var_choice_2_posn= float(split_name_for_org[10])
+        #print(var_choice_2_posn)
+        realisation_posn= int(split_name_for_org[6])
+        #print(realisation_posn)
+        #print(np.where(var_choice_1==var_choice_1_posn))
+        var_choice_1_index= np.where(var_choice_1==var_choice_1_posn)[0][0]
+        #print(var_choice_1_index)
+        var_choice_2_index= np.where(var_choice_2==var_choice_2_posn)[0][0]
+        #print(var_choice_2_index)
+        realisation_index_= np.where(realisation_index==realisation_posn)[0][0]
+        #print(realisation_index_)
         with open(realisation_name_log[i]) as f:
             read_data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) 
             
 
             if read_data.find(general_error_message) != -1:
                 print('true, error found')
-                error_matrix[realisation_index_,var_choice_1_index,var_choice_2_index]=1
+                error_matrix[realisation_index_,var_choice_1_index,var_choice_2_index]=1.0
 
 
             else:
                 print('Successful run')
-                error_matrix[var_choice_1_index,var_choice_2_index,realisation_index_]=0
+                error_matrix[realisation_index_,var_choice_1_index,var_choice_2_index]=0.0
 
     return error_matrix
+
+#%%
 
 error_matrix=log_error_detector(filepath,var_choice_1,var_choice_2,realisation_name_log, count_log)
 
 
-
-# %%
