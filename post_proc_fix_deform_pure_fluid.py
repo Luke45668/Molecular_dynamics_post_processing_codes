8fc8ecb0ee3fbc972f631512ded004b24e2408ac
Luke45668 <luke.debono.21@ucl.ac.uk>
2024-01-12 18:06:18 +0000

    progress

--- post_proc_fix_deform_pure_fluid.py
+++ post_proc_fix_deform_pure_fluid.py
@@ -38,8 +38,8 @@ box_size=23
 #nu_bar=0.9 
 delta_t_srd=0.05071624521210362
 box_vol=box_size**3
-erate=0.002
-no_timesteps=600010
+erate=0.0
+no_timesteps=1650000
 # estimating number of steps  required
 strain=3
 delta_t_md=delta_t_srd/10
@@ -49,9 +49,9 @@ number_steps_needed= np.ceil(strain/(strain_rate*delta_t_md))
 
 #%% importing one log file 
 
-realisation_name = "log.testout_no806324_wall_pure_output_no_rescale_541709_2_60835_23.0_0.005071624521210362_10_10000_10000_600010_T_1_lbda_0.05071624521210362_gdot_0.002" # with shear 
+realisation_name = "log.testout_no806324_wall_pure_output_no_rescale_541709_2_60835_23.0_0.005071624521210362_10_10000_10000_3000000_T_1_lbda_0.05071624521210362_gdot_0" # with shear 
 Path_2_log= "/Volumes/Backup Plus 1/PhD_/Rouse Model simulations/Using LAMMPS imac/Simulation_run_folder/"
-thermo_vars="         KinEng          Temp          TotEng        c_pe[1]        c_pe[2]        c_pe[3]        c_pe[5]        c_mom[1]       c_mom[2]       c_mom[3]       c_mom[5]   "
+thermo_vars="         KinEng          Temp          TotEng        c_pe[1]        c_pe[2]        c_pe[3]        c_pe[5]       c_mom1[1]      c_mom1[2]      c_mom1[3]      c_mom1[5]   "
 log_file_for_test= log2numpy_reader(realisation_name,Path_2_log,thermo_vars)
 #%% Check velocity profiles 
 Path_2_VP=Path_2_log
@@ -77,7 +77,7 @@ plt.show()
 
 
 #%% calculation from log file 
-size_array=122
+size_array=21
 kinetic_energy_tensor_xx= np.array([(log_file_for_test[1:size_array,4]) ]).T
 kinetic_energy_tensor_yy= np.array([(log_file_for_test[1:size_array,5])]).T
 kinetic_energy_tensor_zz= np.array([(log_file_for_test[1:size_array,6])]).T
@@ -156,39 +156,61 @@ print("Pressure",pressure)
 #%% reading a dump file 
 Path_2_dump= Path_2_log
 dump_start_line="ITEM: ATOMS id x y z vx vy vz fx fy fz"
-dump_realisation_name= "testout_541709_2_60835_23.0_0.005071624521210362_10_gdot_0.002_10000_600010_equilibrium_test.dump"
 number_of_particles_per_dump = no_SRD
 
-dump_file= dump2numpy_f(dump_start_line,Path_2_dump,dump_realisation_name,number_of_particles_per_dump)
-#%% reshaping 
-dump_file_1=dump_file[0]
-dump_file_2=dump_file[1]
-dump_file_unsorted=dump_file[2]
 
-loop_size=122
+dump_realisation_name_before= "testout_541709_2_60835_23.0_0.005071624521210362_10_gdot_0_10000_3000000_after_rotation.dump"
+dump_file_before= dump2numpy_f(dump_start_line,Path_2_dump,dump_realisation_name_before,number_of_particles_per_dump)
+dump_realisation_name_after= "testout_541709_2_60835_23.0_0.005071624521210362_10_gdot_0_10000_3000000_before_rotation.dump"
+dump_file_after= dump2numpy_f(dump_start_line,Path_2_dump,dump_realisation_name_after,number_of_particles_per_dump)
+
+
+#%% reshaping 
+
+dump_file_unsorted_before=dump_file_before[2]
+dump_file_unsorted_after=dump_file_after[2]
+
+
+loop_size=165
 columns= 10
 # need to write a test to check this was done properly 
-dump_file_shaped=np.reshape(dump_file_unsorted,(loop_size,number_of_particles_per_dump,columns))
+dump_file_shaped_before=np.reshape(dump_file_unsorted_before,(loop_size,number_of_particles_per_dump,columns))
+dump_file_shaped_after=np.reshape(dump_file_unsorted_after,(loop_size,number_of_particles_per_dump,columns))
 
 #%% sorting rows 
 
-dump_file_sorted=np.zeros((loop_size,number_of_particles_per_dump,columns))
+dump_file_sorted_before=np.zeros((loop_size,number_of_particles_per_dump,columns))
+dump_file_sorted_after=np.zeros((loop_size,number_of_particles_per_dump,columns))
 
 for i in range(0,loop_size):
     for j in range(0,number_of_particles_per_dump):
-        id= int(float(dump_file_shaped[i,j,0]))-1
-        dump_file_sorted[i,id,:]= dump_file_shaped[i,j,:]
+        id= int(float(dump_file_shaped_before[i,j,0]))-1
+        dump_file_sorted_before[i,id,:]= dump_file_shaped_before[i,j,:]
+       
+
+for i in range(0,loop_size):
+    for j in range(0,number_of_particles_per_dump):
+        
+        id= int(float(dump_file_shaped_after[i,j,0]))-1
+        dump_file_sorted_after[i,id,:]= dump_file_shaped_after[i,j,:]
 
 # boolean to check order is correct 
 comparison_list =np.arange(1,number_of_particles_per_dump+1,1)
 error_count=0
 for i in range(0,loop_size):
 
-     boolean_result = dump_file_sorted[i,:,0]==comparison_list
-     if np.all(boolean_result)==True:
+     boolean_result_1 = dump_file_sorted_before[i,:,0]==comparison_list
+     if np.all(boolean_result_1)==True:
          print("success")
      else:
         error_count=error_count+1
+
+     boolean_result_2 = dump_file_sorted_after[i,:,0]==comparison_list
+     if np.all(boolean_result_2)==True:
+         print("success")
+     else:
+        error_count=error_count+1
+
 print(error_count)
      
 #%% now can do calculation
@@ -196,43 +218,64 @@ print(error_count)
 kinetic_energy_tensor=np.zeros((loop_size,4))
 
 for i in range(0,loop_size):
-    kinetic_energy_tensor[i,0]= np.sum(dump_file_sorted[i,:,4]* dump_file_sorted[i,:,4])/box_vol#xx
-    kinetic_energy_tensor[i,1]= np.sum(dump_file_sorted[i,:,5]* dump_file_sorted[i,:,5])/box_vol#yy
-    kinetic_energy_tensor[i,2]= np.sum(dump_file_sorted[i,:,6]* dump_file_sorted[i,:,6])/box_vol#zz
-    kinetic_energy_tensor[i,3]= np.sum(dump_file_sorted[i,:,4]* dump_file_sorted[i,:,6])/box_vol#xz
+    kinetic_energy_tensor[i,0]= np.sum(dump_file_sorted_before[i,:,4]* dump_file_sorted_before[i,:,4])/box_vol#xx
+    kinetic_energy_tensor[i,1]= np.sum(dump_file_sorted_before[i,:,5]* dump_file_sorted_before[i,:,5])/box_vol#yy
+    kinetic_energy_tensor[i,2]= np.sum(dump_file_sorted_before[i,:,6]* dump_file_sorted_before[i,:,6])/box_vol#zz
+    kinetic_energy_tensor[i,3]= np.sum(dump_file_sorted_before[i,:,4]* dump_file_sorted_before[i,:,6])/box_vol#xz
 
 
 
 # pressure= np.mean(np.mean(kinetic_energy_tensor[:,0:2],axis=1))
 
 #%%
-delta_mom_pos_tensor_from_dump=np.zeros((dump_file_sorted.shape[0]-1,no_SRD,4))
-
-
-
+delta_mom_pos_tensor_from_dump=np.zeros((dump_file_sorted_after.shape[0]-1,no_SRD,4))
+mom_pos_tensor_from_dump=np.zeros((dump_file_sorted_after.shape[0]-1,no_SRD,4))
 
 for i in range(0,loop_size-1):
-    delta_mom_pos_tensor_from_dump[i,:,0]=(dump_file_sorted[i+1,:,4]- dump_file_sorted[i,:,4]) *  dump_file_sorted[i,:,1]#xx
-    delta_mom_pos_tensor_from_dump[i,:,1]= (dump_file_sorted[i+1,:,5]- dump_file_sorted[i,:,5]) *  dump_file_sorted[i,:,2]#yy
-    delta_mom_pos_tensor_from_dump[i,:,2]= (dump_file_sorted[i+1,:,6]- dump_file_sorted[i,:,6]) * dump_file_sorted[i,:,3] #zz 
-    delta_mom_pos_tensor_from_dump[i,:,3]= (dump_file_sorted[i+1,:,4]- dump_file_sorted[i,:,4]) *  dump_file_sorted[i,:,3] #xz
+    mom_pos_tensor_from_dump[i,:,0]= dump_file_sorted_after[i,:,4]*  dump_file_sorted_after[i,:,1]#xx
+    mom_pos_tensor_from_dump[i,:,1]= dump_file_sorted_after[i,:,5]*  dump_file_sorted_after[i,:,2]#yy
+    mom_pos_tensor_from_dump[i,:,2]= dump_file_sorted_after[i,:,6]* dump_file_sorted_after[i,:,3] #zz 
+    mom_pos_tensor_from_dump[i,:,3]= dump_file_sorted_after[i,:,4]*  dump_file_sorted_after[i,:,3] #xz
+
+mom_pos_tensor_from_dump=np.mean(mom_pos_tensor_from_dump,axis=1)
+
+
+
+for i in range(1,loop_size-1):
+    delta_mom_pos_tensor_from_dump[i,:,0]=(dump_file_sorted_after[i,:,4]- dump_file_sorted_before[i,:,4]) *  dump_file_sorted_after[i,:,1]#xx
+    delta_mom_pos_tensor_from_dump[i,:,1]= (dump_file_sorted_after[i,:,5]- dump_file_sorted_before[i,:,5]) *  dump_file_sorted_after[i,:,2]#yy
+    delta_mom_pos_tensor_from_dump[i,:,2]= (dump_file_sorted_after[i,:,6]- dump_file_sorted_before[i,:,6]) * dump_file_sorted_after[i,:,3] #zz 
+    delta_mom_pos_tensor_from_dump[i,:,3]= (dump_file_sorted_after[i,:,4]- dump_file_sorted_before[i,:,4]) *  dump_file_sorted_after[i,:,3] #xz
     # delta_mom_pos_tensor_from_dump[i,1]= np.sum(dump_file_sorted[i+1,:,5]- dump_file_sorted[i,:,5] *  dump_file_sorted[i,:,2])#yy
     # delta_mom_pos_tensor_from_dump[i,2]= np.sum(dump_file_sorted[i+1,:,6]- dump_file_sorted[i,:,6] * dump_file_sorted[i,:,3]) #zz 
     # delta_mom_pos_tensor_from_dump[i,3]= np.sum((dump_file_sorted[i+1,:,4]- dump_file_sorted[i,:,4]) *  dump_file_sorted[i,:,3]) #xz
     # print(delta_mom_pos_tensor_from_dump[i,3]/(delta_t_srd*box_vol))
 #delta_mom_pos_tensor_from_dump=delta_mom_pos_tensor_from_dump/(delta_t_srd*box_vol)
-delta_mom_pos_tensor_from_dump_mean=np.mean(np.sum( delta_mom_pos_tensor_from_dump,axis=1)/(delta_t_srd*box_vol),axis=0)/no_SRD
+delta_mom_pos_tensor_from_dump_mean=np.mean(np.sum( delta_mom_pos_tensor_from_dump[1:,:],axis=1),axis=0)/(delta_t_srd*box_vol)
 
-delta_mom_pos_tensor_from_dump=np.sum( delta_mom_pos_tensor_from_dump,axis=1)/(delta_t_srd*box_vol)
+delta_mom_pos_tensor_from_dump_summed=np.sum( delta_mom_pos_tensor_from_dump[1:,:],axis=1)/(delta_t_srd*box_vol)
 
+#%% checking convergence of the sum 
+
+delta_mom_pos_tensor_from_dump_partial_sum = np.zeros((99,4))
+
+for i in range(0,99):
+      delta_mom_pos_tensor_from_dump_partial_sum[i,:]=np.mean(delta_mom_pos_tensor_from_dump_summed[:i,:],axis=0)
+
+
+for i in range(0,3):
+    plt.plot(delta_mom_pos_tensor_from_dump_partial_sum[:,i])
+
+plt.show()
 
 
 #%%
+
 shear_rate_term_from_dump=(erate*delta_t_srd/2)*kinetic_energy_tensor[2:,2]
-stress_tensor_xz_from_dump= kinetic_energy_tensor[2:,3]+ shear_rate_term_from_dump+ delta_mom_pos_tensor_from_dump[1:,3]
-# stress_tensor_xz_rms_mean_from_dump= np.sqrt(np.mean(stress_tensor_xz**2))
-stress_tensor_xz_mean_from_dump= -np.mean(stress_tensor_xz)
-viscosity_from_dump=stress_tensor_xz_mean_from_dump/erate
+# stress_tensor_xz_from_dump= kinetic_energy_tensor[2:,3]+ shear_rate_term_from_dump+ delta_mom_pos_tensor_from_dump[1:,3]
+# # stress_tensor_xz_rms_mean_from_dump= np.sqrt(np.mean(stress_tensor_xz**2))
+# stress_tensor_xz_mean_from_dump= -np.mean(stress_tensor_xz)
+# viscosity_from_dump=stress_tensor_xz_mean_from_dump/erate
 
 stress_tensor_xx_from_dump=( kinetic_energy_tensor[2:,0] +delta_mom_pos_tensor_from_dump[1:,0])
 stress_tensor_yy_from_dump=( kinetic_energy_tensor[2:,1] +delta_mom_pos_tensor_from_dump[1:,1])
